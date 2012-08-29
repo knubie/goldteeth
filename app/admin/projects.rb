2da0dcb62a61d840dc718beccf2f8a20f46778ab
Matt Steedman <matt@tankjetstudios.com>
2012-08-29 13:22:16 -0400

    did some todos, made sort a grid

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -16,7 +16,15 @@ ActiveAdmin.register Project do
       image_tag project.image.url, :width => 100
     end
     column :created_at
-    default_actions
+    column "" do |project|
+      links = link_to "View", admin_project_path(project)
+      links += ' '
+      links += link_to "Edit", edit_admin_project_path(project)
+      links += ' '
+      links += link_to "Delete", admin_project_path(project), :method => :delete
+      links += ' '
+      links += link_to "Sort", sort_admin_project_path(project)
+    end
   end
   form :partial => 'form'
 
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
     "image": false
   });
   $('#sortable-slides').sortable({
-    axis: 'y',
     update: function() {
       $.post("update_sort", $(this).sortable('serialize'))
     }
