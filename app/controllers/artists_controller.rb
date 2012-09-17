0eab1f45711935032f46eda0c9b11956532ac73c
Matt Steedman <matt@tankjetstudios.com>
2012-09-17 10:41:57 -0400

    made artist link

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,7 +6,6 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @page = 'artists'
     @artist = Artist.find_by_name params[:name].gsub('-', ' ').tr('_', ' ').gsub(/\s+/, ' ').gsub(/\b\w/){ $`[-1,1] == "'" ? $& : $&.upcase }
     @projects = @artist.projects.order 'position'
     @recent_work = Post.find_all_by_artist_id(@artist.id)[0..3]
